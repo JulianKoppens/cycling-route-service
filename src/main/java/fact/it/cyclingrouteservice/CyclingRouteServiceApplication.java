c247c732c4c80384a24fe8e4170beec637efaf38
Julian Koppens <r0802564@student.thomasmore.be>
2022-01-05 13:34:09 +0100

    nieuw java project met postgres

---
+++ src/main/java/fact/it/cyclingrouteservice/CyclingRouteServiceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.cyclingrouteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyclingRouteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyclingRouteServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fact/it/cyclingrouteservice/CyclingRouteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.cyclingrouteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyclingRouteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
